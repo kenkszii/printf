dec5848d469fd6fdb84c06aa76fe5f5fded89410
Abdulazeez <azeezgaj@gmail.com>
2022-08-08 00:40:12 -0700

    get precision

--- get_precision.c
+++ get_precision.c
@@ -2,9 +2,9 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
+ * @format: Formatted string in which is to print the arguments
  * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @list: list of the arguments.
  *
  * Return: Precision.
  */
