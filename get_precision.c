ab76723d7c205fcc40785a4761f7c63420f8409d
Abdulazeez <azeezgaj@gmail.com>
2022-08-08 06:06:10 -0700

    Education is when you read the fine print. Experience is what you get if you don't

--- get_precision.c
+++ get_precision.c
@@ -2,9 +2,9 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which is to print the arguments
+ * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: list of the arguments.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
@@ -39,3 +39,5 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
+Footer
+
