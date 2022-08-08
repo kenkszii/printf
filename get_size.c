2bfd7255701a6df9182ff574f814147126cb21ba
Abdulazeez <azeezgaj@gmail.com>
2022-08-08 06:08:35 -0700

    With a face like mine, I do better in print

--- get_size.c
+++ get_size.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the arguments
+ * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
- * Returns: Precision.
+ * Return: Precision.
  */
 int get_size(const char *format, int *i)
 {
@@ -24,3 +24,5 @@ int get_size(const char *format, int *i)
 
 	return (size);
 }
+Footer
+
